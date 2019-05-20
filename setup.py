18a9b04a271fdf35959e259788e4dcaaaa112c8a
Nicolas Bock <nicolasbock@gmail.com>
2019-05-20 05:03:37 -0600

    Get version from latest git tag

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import setuptools
+import subprocess
 
 
 def readme():
@@ -8,9 +9,24 @@ def readme():
         return fd.read()
 
 
+def get_latest_tag():
+    git = subprocess.Popen(['git', 'describe', '--tags'],
+                           stdout=subprocess.PIPE,
+                           universal_newlines=True)
+    git.wait()
+    if git.returncode == 0:
+        raw_version = git.stdout.readlines()[0][1:].rstrip().split('-')
+        version = raw_version[0]
+        if len(version) > 1:
+            version += ".dev" + raw_version[1]
+        return version
+    else:
+        return "unknown"
+
+
 setuptools.setup(
     name="filechooser",
-    version="0.1.6",
+    version=get_latest_tag(),
     author="Nicolas Bock",
     author_email="nicolasbock@gmail.com",
     description="A script that copies a random selection of files from "
