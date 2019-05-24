1637cbcb0b9a6b436929726977b012dc70cb5b13
Nicolas Bock <nicolasbock@gmail.com>
2019-05-24 05:37:08 -0600

    Fix bug in version detection

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_latest_tag():
     if git.returncode == 0:
         raw_version = git.stdout.readlines()[0][1:].rstrip().split('-')
         version = raw_version[0]
-        if len(version) > 1:
+        if len(raw_version) > 1:
             version += ".dev" + raw_version[1]
         return version
     else:
