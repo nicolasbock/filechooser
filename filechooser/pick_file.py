64588eec675675eee542bfba82c439290833cf0c
Nicolas Bock <nicolasbock@gmail.com>
2019-09-08 07:28:55 -0600

    Add unit testing of file picking
    This change adds a function that recursively picks image files from a
    list of paths and returns a list of image files found. Unit tests are
    added to test this new function.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- filechooser/pick_file.py
+++ filechooser/pick_file.py
@@ -1,3 +1,13 @@
+import mimetypes
+import os.path
+
+image_file_extensions = [
+    "image/jpeg",
+    "image/gif",
+    "image/png",
+]
+
+
 def get_image_files(paths):
     # type: (List[str]) -> List[str]
     """Get a list of supported image files under a list of paths.
@@ -7,4 +17,25 @@ def get_image_files(paths):
     paths).
 
     """
-    return ["a/picture_a.jpg", "b/picture_b.jpg"]
+
+    print("called with {}".format(paths))
+    result = []
+    for path in paths:
+        print("Checking {}".format(path))
+        if not os.path.exists(path):
+            raise Exception("The path {} does not exist".format(path))
+        if os.path.isdir(path):
+            new_paths = [os.path.join(path, p) for p in os.listdir(
+                path) if p not in ['.', '..', path]]
+            result += get_image_files(new_paths)
+        else:
+            filetype = mimetypes.guess_type("file://" + path)
+            print("checking file {} {}".format(path, filetype))
+            if filetype[0] in image_file_extensions:
+                print("adding image file {}".format(path))
+                result.append(path)
+            else:
+                print("file {} {} does not have a supported type".format(
+                    path, filetype))
+    print("returning {}".format(result))
+    return result
--- tests/test_pick.py
+++ tests/test_pick.py
@@ -1,16 +1,52 @@
+import os
+import pathlib
+import tempfile
 import unittest
+
 from filechooser import pick_file
+
+images = [
+    "collection_a/a/a_1.jpg",
+    "collection_a/a/a_2.gif",
+    "collection_a/b/b_1.jpg",
+    "collection_b/c/c_1.jpg",
+]
 
 
 class TestPick(unittest.TestCase):
 
-    def setup(self):
-        pass
+    def setUp(self):
+        # Create a filesystem structure in a temporary location. This
+        # should eventually be replaced with something in memory, i.e.
+        # using `pyfakefs`.
+        print("setUp")
+
+        self.fs_base = tempfile.mkdtemp()
+        print("fs_base: {}".format(self.fs_base))
+
+        for image in images:
+            path = os.path.dirname(image)
+            print("creating path = {} for {}".format(path, image))
+            try:
+                os.makedirs(os.path.join(self.fs_base, path), exist_ok=True)
+            except FileExistsError as e:
+                print("Can not create path {}".format(
+                    os.path.join(self.fs_base, path)))
+                raise e
+            pathlib.Path(os.path.join(self.fs_base, image)).touch()
 
     def tearDown(self):
+        print("tearDown")
         pass
 
     def test_get_image_files(self):
-        self.assertEqual(pick_file.get_image_files(
-            ["a", "b"]), ["a/picture_a.jpg", "b/picture_b.jpg"])
-        assert(True)
+        image_files = sorted(pick_file.get_image_files(
+            [os.path.join(self.fs_base, "collection_a"),
+             os.path.join(self.fs_base, "collection_b")]))
+        reference_image_files = sorted(
+            [os.path.join(self.fs_base, p) for p in images])
+        self.assertEqual(image_files, reference_image_files)
+
+    def test_get_image_files_not_exist(self):
+        with self.assertRaisesRegex(Exception, "does not exist"):
+            pick_file.get_image_files(["does_not_exist"])
