85fd56a7d2965f0904debc84f8aa33cadceb10a0
Nicolas Bock <nicolasbock@gmail.com>
2019-09-09 12:18:59 -0600

    Also test for unsupported filetypes
    Add a file with an unsupported file extension (".txt") to test whether
    the `pick_file` function correctly ignores this file.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- tests/test_pick.py
+++ tests/test_pick.py
@@ -10,6 +10,7 @@ images = [
     "collection_a/a/a_1.jpg",
     "collection_a/a/a_2.gif",
     "collection_a/b/b_1.jpg",
+    "collection_a/b/b_2.txt",
     "collection_b/c/c_1.jpg",
 ]
 
@@ -35,11 +36,12 @@ class TestPick(unittest.TestCase):
         shutil.rmtree(self.fs_base)
 
     def test_get_image_files(self):
+        # Sort the lists so we can compare them directly.
         image_files = sorted(pick_file.get_image_files(
             [os.path.join(self.fs_base, "collection_a"),
              os.path.join(self.fs_base, "collection_b")]))
         reference_image_files = sorted(
-            [os.path.join(self.fs_base, p) for p in images])
+            [os.path.join(self.fs_base, p) for p in images if p.split(".")[-1] != "txt"])
         self.assertEqual(image_files, reference_image_files)
 
     def test_get_image_files_not_exist(self):
