702cd916c3aa8807a1efd35b96c99c76e28b0c38
Nicolas Bock <nicolasbock@gmail.com>
2019-10-14 19:40:02 -0600

    Remove `timestamp` argument
    The timestamp is always the current time.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,6 +2,11 @@ import os
 import tempfile
 import unittest
 
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
 import filechooser.db as db
 from filechooser.logger import logger
 
@@ -20,14 +25,18 @@ class TestDB(unittest.TestCase):
         os.remove(self.db.name)
 
     def test_set_timestamp(self):
-        db.set_timestamp("a.gif", "timestamp1")
+        with mock.patch("filechooser.db.time", return_value=1.0):
+            db.set_timestamp("a.gif")
         result = db.get_timestamp("a.gif")
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0]['filename'], "a.gif")
+        self.assertEqual(result[0]['timestamp'], 1.0)
 
     def test_update_timestamp(self):
-        db.set_timestamp("b.gif", "timestamp1")
-        db.set_timestamp("b.gif", "timestamp2")
+        with mock.patch("filechooser.db.time", return_value=1.0):
+            db.set_timestamp("b.gif")
+        with mock.patch("filechooser.db.time", return_value=2.0):
+            db.set_timestamp("b.gif")
         result = db.get_timestamp("b.gif")
         self.assertEqual(len(result), 1)
-        self.assertEqual(result[0]['timestamp'], "timestamp2")
+        self.assertEqual(result[0]['timestamp'], 2.0)
