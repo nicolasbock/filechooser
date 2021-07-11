3a3b031f3613ae3dfb4d5db23e6cb62da753f5dd
Nicolas Bock <nicolasbock@gmail.com>
2021-07-11 09:01:41 +0200

    Refactor version function into module
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- setup.py
+++ setup.py
@@ -1,36 +1,12 @@
 #!/usr/bin/env python
 
-import os.path
 import setuptools
-import subprocess
-
-
-def get_latest_tag():
-    version = "unknown"
-    try:
-        with open('.version') as fd:
-            version = fd.readline()
-        return version
-    except FileNotFoundError:
-        pass
-
-    git = subprocess.Popen(['git', 'describe', '--tags'],
-                           stdout=subprocess.PIPE,
-                           universal_newlines=True)
-    git.wait()
-    if git.returncode == 0:
-        raw_version = git.stdout.readlines()[0][1:].rstrip().split('-')
-        version = raw_version[0]
-        if len(raw_version) > 1:
-            version += ".dev" + raw_version[1]
-    with open('.version', 'w') as fd:
-        fd.write('{}\n'.format(version))
-    return version
+from filechooser import __version__
 
 
 setuptools.setup(
     name="filechooser",
-    version=get_latest_tag(),
+    version=__version__,
     data_files=['.version'],
     license="BSD",
     url="https://github.com/nicolasbock/filechooser.git",
