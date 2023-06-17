6477f34025fc2f7e0a74bf761041c7704580fd73
Nicolas Bock <nicolasbock@gmail.com>
2023-06-16 21:19:32 -0700

    Add first test
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- filechooser_test.go
+++ filechooser_test.go
@@ -2,6 +2,23 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {
-
+func TestReadFiles1(t *testing.T) {
+	want := []File{
+		{
+			name: "a.txt",
+			path: "artifacts/b/b/a.txt",
+			md5sum: "572cdaabff13f80efd9acd74e00fad4a",
+		},
+	}
+	got := readFiles([]string{"artifacts/b/b"})
+	passed := true
+	if len(want) != len(got) {
+		passed = false
+	}
+	if got[0] != want[0] {
+		passed = false
+	}
+	if ! passed {
+		t.Errorf("want: %s != got: %s\n", want, got)
+	}
 }
