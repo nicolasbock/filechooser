d5ec06786de9393f52c6c822c99dbc6ccd057823
Nicolas Bock <nicolasbock@gmail.com>
2023-06-22 22:23:52 +0200

    Add support for suffix filter
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- pick-files.go
+++ pick-files.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
 	"os"
 	"path"
+	"regexp"
 	"strings"
 	"time"
 
@@ -19,6 +20,7 @@ import (
 var Version = "unknown"
 
 type Folders []string
+type Suffixes []string
 
 func (f *Folders) Set(s string) error {
 	*f = append(*f, s)
@@ -26,7 +28,17 @@ func (f *Folders) Set(s string) error {
 }
 
 func (f *Folders) String() string {
-	return strings.Join(*f, ",")
+	return strings.Join(*f, ", ")
+}
+
+// Set will append a new suffix and remove a leading '.'.
+func (f *Suffixes) Set(s string) error {
+	*f = append(*f, regexp.MustCompile("^[.]+").ReplaceAllString(s, ""))
+	return nil
+}
+
+func (f *Suffixes) String() string {
+	return strings.Join(*f, ", ")
 }
 
 // File represents a regular file in the source folders.
@@ -61,7 +73,7 @@ type ProgramOptions struct {
 	n              int
 	output         string
 	printVersion   bool
-	suffix         string
+	suffixes       Suffixes
 }
 
 var options ProgramOptions = ProgramOptions{}
@@ -74,7 +86,7 @@ pick-files is a script that copies a random selection of files from a set of fol
 
 # Usage Example
 
-pick-files --number 20 --destination new_folder --suffix .jpg .avi --folder folder1 --folder folder2
+pick-files --number 20 --destination new_folder --suffix .jpg --suffix .avi --folder folder1 --folder folder2
 
 Would choose at random 20 files from folder1 and folder2 (including sub-folders) and copy those files into new_folder. The new_folder is created if it does not exist already. In this example, only files with matching suffixes .jpg and .avi are considered.
 
@@ -97,7 +109,7 @@ func parseCommandline() {
 	gnuflag.StringVar(&options.output, "destination", "output", "The output PATH for the "+
 		"selected files.")
 	gnuflag.BoolVar(&options.printVersion, "version", false, "Print the version of this program.")
-	gnuflag.StringVar(&options.suffix, "suffix", "", "Only consider files with this SUFFIX. For instance, to only load "+
+	gnuflag.Var(&options.suffixes, "suffix", "Only consider files with this SUFFIX. For instance, to only load "+
 		"jpeg files you would specify either 'jpg' or '.jpg'. By default, all files are be considered."+
 		"The suffix is case insensitive.")
 	gnuflag.BoolVar(&options.helpRequested, "h", false, "This help message.")
@@ -178,15 +190,24 @@ func copyFile(src, dst string) (int64, error) {
 func pickFiles() {
 	var allFiles = readFiles(options.folders)
 	var pickedFileIndices = []int{}
-	var allFileIndices = make([]int, len(allFiles))
+	var allFileIndices = []int{}
+	var suffixRegex = ".*$"
+
+	if len(options.suffixes) > 0 {
+		suffixRegex = "[.](" + strings.Join(options.suffixes, "|") + ")$"
+	}
+	var re = regexp.MustCompile(suffixRegex)
 
 	for i := 0; i < len(allFiles); i++ {
-		allFileIndices[i] = i
+		if re.MatchString(allFiles[i].path) {
+			allFileIndices = append(allFileIndices, i)
+		}
 	}
 
 	for i := 0; i < options.n; i++ {
-		if len(allFiles) == 0 {
-			break
+		if len(allFileIndices) == 0 {
+			log.Warn().Msg("Could not find any files")
+			return
 		}
 		j := rand.Intn(len(allFileIndices))
 		pickedFileIndices = append(pickedFileIndices, allFileIndices[j])
@@ -240,8 +261,8 @@ func main() {
 	}
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	log.Info().Msgf("Will pick %d file(s) randomly", options.n)
-	log.Info().Msgf("Source folders: %s", &options.folders)
+	log.Info().Msgf("Will pick %d file(s) randomly matching suffixes %s", options.n, options.suffixes.String())
+	log.Info().Msgf("Source folders: %s", options.folders.String())
 	log.Info().Msgf("The selected files will go into the '%s' folder", options.output)
 
 	pickFiles()
