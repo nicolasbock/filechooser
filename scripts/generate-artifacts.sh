59bc5758a840cf1e57eb2ee689dfb47a74980316
Nicolas Bock <nicolasbock@gmail.com>
2023-08-24 07:47:40 -0600

    Make generate script a bit more flexible
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- scripts/generate-artifacts.sh
+++ scripts/generate-artifacts.sh
@@ -2,13 +2,17 @@
 
 set -e -u -x
 
+basedir=$(realpath $(dirname $0)/..)
+
+: ${DESTINATION:=${basedir}/artifacts}
+
 for i in a b c; do
   for j in a b c d; do
-    mkdir -p artifacts/${i}/${j}
+    mkdir -p ${DESTINATION}/${i}/${j}
     for k in $(seq 10); do
-      for s in jpg png tif; do
+      for s in jpg png tif jpeg; do
         UUID=$(uuidgen)
-        echo ${UUID} > artifacts/${i}/${j}/$(printf "%03d-${UUID}.${s}" ${k})
+        echo ${UUID} > ${DESTINATION}/${i}/${j}/$(printf "%03d-${UUID}.${s}" ${k})
       done
     done
   done
